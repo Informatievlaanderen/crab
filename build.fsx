fe336a3087fee50cb67a6ce72783c7c5cb1d3a9a
Wouter De Rijck <wouter.derijck@kb.vlaanderen.be>
2019-11-12 15:19:50 +0100

    BREAKING CHANGE: upgrade to core 3.0

--- build.fsx
+++ build.fsx
@@ -1,36 +1,48 @@
+#r "paket:
+version 5.226.0
+framework: netstandard20
+source https://api.nuget.org/v3/index.json
+nuget Be.Vlaanderen.Basisregisters.Build.Pipeline 3.0.0 //"
+
 #load "packages/Be.Vlaanderen.Basisregisters.Build.Pipeline/Content/build-generic.fsx"
 
-open Fake
+open Fake.Core
+open Fake.Core.TargetOperators
+open Fake.IO.FileSystemOperators
 open ``Build-generic``
 
 let assemblyVersionNumber = (sprintf "%s.0")
 let nugetVersionNumber = (sprintf "%s")
 
 let build = buildSolution assemblyVersionNumber
+let test = testSolution
 let publish = publishSolution assemblyVersionNumber
 let pack = packSolution nugetVersionNumber
 
 // Library ------------------------------------------------------------------------
 
-Target "Lib_Build" (fun _ -> build "Be.Vlaanderen.Basisregisters.Crab")
-
-Target "Lib_Publish" (fun _ -> publish "Be.Vlaanderen.Basisregisters.Crab")
-Target "Lib_Pack" (fun _ -> pack "Be.Vlaanderen.Basisregisters.Crab")
+Target.create "Lib_Build" (fun _ -> build "Be.Vlaanderen.Basisregisters.Crab")
+Target.create "Lib_Test" (fun _ -> test "Be.Vlaanderen.Basisregisters.Crab")
+Target.create "Lib_Publish" (fun _ -> publish "Be.Vlaanderen.Basisregisters.Crab")
+Target.create "Lib_Pack" (fun _ -> pack "Be.Vlaanderen.Basisregisters.Crab")
 
 // --------------------------------------------------------------------------------
 
-Target "PublishLibrary" DoNothing
-Target "PublishAll" DoNothing
+Target.create "Test" ignore
 
-Target "PackageMyGet" DoNothing
-Target "PackageAll" DoNothing
+Target.create "PublishLibrary" ignore
+Target.create "PublishAll" ignore
+
+Target.create "PackageMyGet" ignore
+Target.create "PackageAll" ignore
 
 // Publish ends up with artifacts in the build folder
-"DotNetCli" ==> "Clean" ==> "Restore" ==> "Lib_Build" ==> "Lib_Publish" ==> "PublishLibrary"
+"DotNetCli" ==> "Clean" ==> "Restore" ==> "Lib_Build" ==> "Lib_Test" ==> "Lib_Publish" ==> "PublishLibrary"
+"Lib_Test" ==> "Test"
 "PublishLibrary" ==> "PublishAll"
 
 // Package ends up with local NuGet packages
 "PublishLibrary" ==> "Lib_Pack" ==> "PackageMyGet"
 "PackageMyGet" ==> "PackageAll"
 
-RunTargetOrDefault "Lib_Build"
+Target.runOrDefault "Lib_Build"
