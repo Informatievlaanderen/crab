575ed51e4c0012c3758187a6228c04d49506d279
Wouter De Rijck <wouter.derijck@vlaanderen.be>
2020-05-13 18:15:20 +0200

    fix: move to GH-actions

--- build.fsx
+++ build.fsx
@@ -2,13 +2,12 @@
 version 5.241.6
 framework: netstandard20
 source https://api.nuget.org/v3/index.json
-nuget Be.Vlaanderen.Basisregisters.Build.Pipeline 3.3.2 //"
+nuget Be.Vlaanderen.Basisregisters.Build.Pipeline 4.0.6 //"
 
 #load "packages/Be.Vlaanderen.Basisregisters.Build.Pipeline/Content/build-generic.fsx"
 
 open Fake.Core
 open Fake.Core.TargetOperators
-open Fake.IO.FileSystemOperators
 open ``Build-generic``
 
 let assemblyVersionNumber = (sprintf "%s.0")
@@ -18,27 +17,28 @@ let build = buildSolution assemblyVersionNumber
 let publish = publishSolution assemblyVersionNumber
 let pack = packSolution nugetVersionNumber
 
+supportedRuntimeIdentifiers <- [ "linux-x64" ]
+
 // Library ------------------------------------------------------------------------
-
 Target.create "Lib_Build" (fun _ -> build "Be.Vlaanderen.Basisregisters.Crab")
-
 Target.create "Lib_Publish" (fun _ -> publish "Be.Vlaanderen.Basisregisters.Crab")
 Target.create "Lib_Pack" (fun _ -> pack "Be.Vlaanderen.Basisregisters.Crab")
 
 // --------------------------------------------------------------------------------
-
-Target.create "PublishLibrary" ignore
 Target.create "PublishAll" ignore
-
-Target.create "PackageMyGet" ignore
 Target.create "PackageAll" ignore
 
 // Publish ends up with artifacts in the build folder
-"DotNetCli" ==> "Clean" ==> "Restore" ==> "Lib_Build" ==> "Lib_Publish" ==> "PublishLibrary"
-"PublishLibrary" ==> "PublishAll"
+"DotNetCli"
+==> "Clean"
+==> "Restore"
+==> "Lib_Build"
+==> "Lib_Publish"
+==> "PublishAll"
 
 // Package ends up with local NuGet packages
-"PublishLibrary" ==> "Lib_Pack" ==> "PackageMyGet"
-"PackageMyGet" ==> "PackageAll"
+"PublishAll"
+==> "Lib_Pack"
+==> "PackageAll"
 
 Target.runOrDefault "Lib_Build"
